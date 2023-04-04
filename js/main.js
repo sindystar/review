4f7a95d97d70a141a62c63f1baf8fb408fe3c7f6
sindystar <hasungah@gmail.com>
2023-04-04 23:04:04 +0900

    절차 지향

--- js/main.js
+++ js/main.js
@@ -3,68 +3,47 @@ const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const lis = ul.querySelectorAll("li");
 let len = lis.length; //추가가 되도 자동 li의 갯수를 세어줌 
-let enableClick = true;
-let speed = 500;
 
-//초기화 함수를 호출 
-init();
+ul.style.left = "-100%"; //ul의 초기 위치값을 정해주는 코드 
+ul.prepend(ul.lastElementChild); //로딩후 첫번째 li가 frame에 보이도록 초기화 하는 코드
 
-//next 슬라이드 이동 코드 
+//ul의 너비값을 li의 갯수에 맞춰서 자동 계산 코드
+ul.style.width = `${100 * len}%`;
+//각 li의 너비값을 자동 계산 해주는 코드 
+lis.forEach((el) => {
+  el.style.left = `${100 / len}%`;
+})
+
+
+//next 슬라이드 이동 코드
 next.addEventListener("click", (e) => {
   e.preventDefault();
 
-  //재이벤트 방지 구문 (재클릭 방지)
-  if(enableClick) {
+  new Anim(ul, {
+    prop: 'left',
+    value: "-200%",
+    duration: 1000,
+    callback: () => {
 
-    nextSlide();
-    enableClick = false;
-    // 함수 호출과 함께 false로 바꾸면서 모션 진행 시간을 보호해 준다 
-    // 그 시간동안은 버튼은 무력화된 상태
-  }
+      ul.append(ul.firstElementChild);  //첫번째 li를 뒤쪽으로 보내는 코드 
+      ul.style.left = "-100%"; // -200%에서 초기 위치 값으로 복귀 하도록 하는 코드 
+    }
+  })
 })
 
 //prev 슬라이드 이동 코드 
 prev.addEventListener("click", (e) => {
   e.preventDefault();
 
-  if (enableClick) {
-    prevSlide();
-    enableClick = false;
-  }
+  new Anim(ul, {
+    prop: "left",
+    value: "0%",
+    duration:1000,
+    callback: () => {
+      ul.style.left = "-100%"; // 0%에서 초기 위치 값으로 복귀하는 코드 
+      ul.prepend(ul.lastElementChild); //마지막 li를 맨앞으로 보내는 코드 
+    }
+  })
 })
 
 
-function init() {
-  ul.style.left = "-100%";
-  ul.prepend(ul.lastElementChild);
-  ul.style.width = `${100 * len}%`;
-  lis.forEach((el) => { el.style.width = `${100 / len}%`; })
-}
-//next 슬라이드 함수 정의 
-function nextSlide() {
-  new Anim(ul, {
-    prop: 'left',
-    value: "-200%",
-    duration: speed,
-    callback: () => {
-
-      ul.append(ul.firstElementChild);  //첫번째 li를 뒤쪽으로 보내는 코드 
-      ul.style.left = "-100%"; // -200%에서 초기 위치 값으로 복귀 하도록 하는 코드 
-      enableClick = true;
-      //모션이 끝나고 다시 true바꾸면서 이벤트가 가능 하도록 한다 
-    }
-  })
-}
-
-function prevSlide() {
-  new Anim(ul, {
-    prop: 'left',
-    value: "0%",
-    duration: speed,
-    callback: () => {
-      ul.style.left = "-100%"; //0%에서 초기 위치 값으로 복귀 하는 코드 
-      ul.prepend(ul.lastElementChild); //마지막 li를 맨앞으로 보내는 코드
-      enableClick = true;
-    }
-  })
-}
